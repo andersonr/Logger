e6d82cbacf652b4f7c6141e1aa6b6a518658d79c
andersonr <anderson.rissardi@hotmail.com>
2019-11-23 15:11:50 -0300

    Finalizando autenticação com Google Auth

--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+
+require('./models/Users');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 259200000,//Tempo em milisegundos - Representa 1 mês
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
